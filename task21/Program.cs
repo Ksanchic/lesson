3ff95a63707ca8d75bc572ced35d7a45bb5997ec
ksanchic <ksanchic@mail.ru>
2022-12-08 23:08:28 +0300

    task21

---
+++ task21/Program.cs
@@ -0,0 +1,18 @@
+﻿//  программа, которая принимает на вход координаты двух точек и находит 
+//расстояние между ними в 3 Dпространстве
+
+{
+double ab =0;
+double[] point = new double[6];                  // 0 1 2      3 4 5
+Console.WriteLine ("Введите коордитаны двух точек A(x,y,z), B(x,y,z):");
+   
+  
+for (int i=0; i < point.Length; i++)
+{
+    point[i]=Convert.ToDouble (Console.ReadLine());
+    ab = Math.Sqrt (((point[3] - point[0]) * (point[3] - point[0])) + ((point[4] - point[1]) * (point[4] - point[1])) + ((point[5] - point[2]) * (point[5] - point[2])));
+}
+Console.Write("ab=");
+Console.WriteLine(ab);
+
+}
